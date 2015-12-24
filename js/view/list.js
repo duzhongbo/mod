a8194661a324d129faeca400cf1791404be8688a
duzhongbo <710409599@qq.com>
2015-12-24 13:59:22 +0800

    去除列表页视图模块冗余代码

--- js/view/list.js
+++ js/view/list.js
@@ -8,16 +8,6 @@ var List = Backbone.View.extend({
   	$('.list').html(this.template(oData));
   },
   template:_.template($('#J_list').html()),
-  el:$('.list'),
-  events:{
-    'click .article-list-a':'showArticles'
-  },
-  showArticles:function(e){
-    var target = e.target || e.srcElement;
-    var url = $(target).attr('href');
-    location.hash=url;
-    return false;
-  }
 });
 
 exports.List = List;
