bea75f1690947f2d08139115ab6a0593010f4441
duzhongbo <710409599@qq.com>
2015-12-24 19:17:39 +0800

    去除标签页视图冗余代码(去之前点击之后重复执行若干次回调，暂时不知道原因)

--- js/view/tag.js
+++ js/view/tag.js
@@ -8,16 +8,6 @@ var Tag = Backbone.View.extend({
   	$('.tags').html(this.template(oData));
   },
   template:_.template($('#J_tag').html()),
-  el:$('.tags'),
-  events:{
-  	'click .tags-a':'showCur'
-  },
-  showCur:function(e){
-  	$('.tags-a').removeClass('tags-cur-a');
-  	var target = e.target || e.srcElement;
-  	console.log(target,12212121,$(target));
-  	$(target).addClass('tags-cur-a');
-  }
 });
 
 exports.Tag = Tag;
