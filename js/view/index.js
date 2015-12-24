b4c2b0b7c5f5f8ad3418549fa2d63f0d6282bc56
duzhongbo <710409599@qq.com>
2015-12-24 13:58:40 +0800

    去除首页视图模块冗余代码

--- js/view/index.js
+++ js/view/index.js
@@ -8,16 +8,6 @@ var Index = Backbone.View.extend({
   	$('.index').html(this.template(oData));
   },
   template:_.template($('#J_index').html()),
-  el:$('.index'),
-  events:{
-    'click .article-list-a':'showArticles'
-  },
-  showArticles:function(e){
-    var target = e.target || e.srcElement;
-    var url = $(target).attr('href');
-    location.hash=url;
-    return false;
-  },
   html_decode:function (str) {// 将已经转义的html标签重新转回
     var s = "";   
     if (str.length == 0) return "";   
