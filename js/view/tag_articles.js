d45d8da500906f955b812b7caf578719e3dfd7f7
duzhongbo <710409599@qq.com>
2015-12-24 19:24:17 +0800

    去除标签视图和其子视图冗余代码

--- js/view/tag_articles.js
+++ js/view/tag_articles.js
@@ -9,16 +9,6 @@ var TagArticles = Backbone.View.extend({
     $('.tags-articles').html(this.template(this.model.toJSON()));
   },
   template:_.template($('#J_tag_articles').html()),
-  el:$('.tags-articles'),
-  events:{
-    'click .tags-articles-a':'showArticles'
-  },
-  showArticles:function(e){
-    var target = e.target || e.srcElement;
-    var url = $(target).attr('href');
-    location.hash=url;
-    return false;
-  }
 });
 
 exports.TagArticles = TagArticles;
