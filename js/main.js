dcaca1fa550fed7c128f1bfb41321b3f91801435
duzhongbo <710409599@qq.com>
2015-12-24 22:58:20 +0800

    代码入口，视图和模型类实例化代码整理

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ define(function(require, exports) {
 	var temp;
 	// 导航
 	temp = require('./view/nav.js');
-	var n = new temp.Nav;
+	new temp.Nav;
 
 
 	// 搜索
 	temp = require('./view/search.js');
-	var s = new temp.Search;
+	new temp.Search;
 
 	// 搜街结果模型
 	temp = require('./model/search_result.js');
@@ -15,27 +15,27 @@ define(function(require, exports) {
 
 	// 搜索结果页
 	temp = require('./view/search_result.js');
-	var s = new temp.SearchResult({model:ms});
+	new temp.SearchResult({model:ms});
 
 
 	// 欢迎页图文切换
 	temp = require('./view/welcome.js');
-	var w = new temp.Welcome;
+	new temp.Welcome;
 
 	// 首页视图
 	temp = require('./view/index.js');
 	var Index = temp.Index;
-	var i = new Index;
+	new Index;
 
 	// 列表页视图
 	temp = require('./view/list.js');
 	var List = temp.List;
-	var l = new List;
+	new List;
 
 	// 标签页视图
 	temp = require('./view/tag.js');
 	var Tag = temp.Tag;
-	var t = new Tag;
+	new Tag;
 
 
 	// 标签页文章列表模型
@@ -46,16 +46,16 @@ define(function(require, exports) {
 	// 标签页文章列表视图
 	temp = require('./view/tag_articles.js');
 	var TagArticles = temp.TagArticles;
-	var t = new TagArticles({model:m});
+	new TagArticles({model:m});
 
 	// 标签页标签部分
 	temp = require('./view/tag.js');
 	var Tag = temp.Tag;
-	var t = new Tag;
+	new Tag;
 
 	// 路由
 	temp = require('./router.js');
-	var r = new temp.Router;
+	new temp.Router;
 	Backbone.history.start();
 
 });
